50f41bb6601af777bdad36d3878970a71e19bc71
CJ Ellis <cellis@talentpath.com>
2019-10-29 10:17:47 -0500

    Started practice mode skeleton

---
+++ src/PracticeMode.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class PracticeMode extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { something: null };
+	}
+	render() {
+		return (
+			<div>
+				<div id="enemy" className="area">
+					<div className="special-move">Special Move</div>
+					<div id="enemy-sprite" className="sprite">
+						Enemy Sprite
+					</div>
+					<div id="player-health" className="health">
+						Health
+					</div>
+				</div>
+				<div id="key-prompt">Q</div>
+				<div id="player" className="area">
+					<div className="special-move">Special Move</div>
+					<div id="player-sprite" className="sprite">
+						Player Sprite
+					</div>
+					<div id="player-health" className="health">
+						Health
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default PracticeMode;
