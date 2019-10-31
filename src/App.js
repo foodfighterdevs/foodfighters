500e097e6a89c4d754999c7ddd8c1c0e0f70220f
CJ Ellis <cellis@talentpath.com>
2019-10-31 13:55:52 -0500

    Added login page boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import PracticeMode from "./PracticeMode";
+import LoginPage from "./LoginPage";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<PracticeMode />
+				{/* <PracticeMode /> */}
+				<LoginPage />
 			</header>
 		</div>
 	);
--- src/PracticeMode.js
+++ src/PracticeMode.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./PracticeMode.css";
 
 class PracticeMode extends React.Component {
 	constructor(props) {
