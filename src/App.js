d7fdf6b5d2915fd3c8ad6a2d3c18ddc1b912c30e
Danny Betancourt <dbetancourt@talentpath.com>
2019-11-04 08:07:29 -0600

    Added sandbox area and incorporated health bar. Need to integrate to player component.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 import PracticeMode from "./PracticeMode";
 
-function App() {
+import
+{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+function App()
+{
 	return (
 		<div className="App">
 			<header className="App-header">
