15d26b6528c3f98df9ff3157c8a837799a09f0c9
Danny Betancourt <dbetancourt@talentpath.com>
2019-11-05 09:25:32 -0600

    Sending to Juan to test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import "./App.css";
-import PracticeMode from "./PracticeMode";
-
-import
-{
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
+import PracticeMode from "./PracticeMode"
 
 function App()
 {
